63a45e7e60033c746c74f00314f8c1e31a0e90a2
Joao <jpapinto@ua.pt>
2024-11-01 17:04:35 +0000

    [feature]: PlayerSession

--- src/backend/core-app/src/main/java/sts/backend/core_app/models/PlayerSession.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/models/PlayerSession.java
@@ -1,5 +1,57 @@
 package sts.backend.core_app.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity(name = "playerSessions")
 public class PlayerSession {
     
+    @ManyToOne
+    @JoinColumn(name = "playerId", nullable = false)
+    private Player player;
+
+    @ManyToOne
+    @JoinColumn(name = "sessionId", nullable = false)
+    private Session session;
+
+    private Long sensorId;
+    
+    // standard constructors / setters / getters / toString
+    public PlayerSession() {}
+
+    public PlayerSession(Player player, Session session, Long sensorId) {
+        this.player = player;
+        this.session = session;
+        this.sensorId = sensorId;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public Long getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(Long sensorId) {
+        this.sensorId = sensorId;
+    }
+
 }
