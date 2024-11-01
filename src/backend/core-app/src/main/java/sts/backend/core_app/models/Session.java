a3fda2bb47c4464b23faa8afe0033ca0e18b02a0
Pedro Pinto <pmap@ua.pt>
2024-11-01 21:37:44 +0000

    remove unnecessary imports

--- src/backend/core-app/src/main/java/sts/backend/core_app/models/Session.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/models/Session.java
@@ -10,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Entity(name = "sessions")
 public class Session {
