78eec7f4d621be30ffd69c64c6dc9b6011235f0a
Joao <jpapinto@ua.pt>
2024-11-01 19:30:25 +0000

    [feature]: Starting TeamDirector and Trainers

--- src/backend/core-app/src/main/java/sts/backend/core_app/models/Trainers.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/models/Trainers.java
@@ -1,10 +1,9 @@
 package sts.backend.core_app.models;
 
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
-public class TeamDirector {
+public class Trainers {
     @OneToOne
     @JoinColumn(name = "userId", nullable = false)
     private User user;
@@ -13,7 +12,7 @@ public class TeamDirector {
     @JoinColumn(name = "teamId", nullable = false)
     private Team team;
 
-    public TeamDirector(User user, Team team) {
+    public Trainers(User user, Team team) {
         this.user = user;
         this.team = team;
     }
