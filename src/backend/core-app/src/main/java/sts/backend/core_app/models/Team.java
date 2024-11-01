d5f57d2a426974da5f32cc868e9a04dba8e507f4
Pedro Pinto <pmap@ua.pt>
2024-11-01 17:05:34 +0000

    feature: add team model

--- src/backend/core-app/src/main/java/sts/backend/core_app/models/Team.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/models/Team.java
@@ -12,12 +12,41 @@ public class Team {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long teamId;
+    private Long teamId;
 
     @NotBlank(message = "Team: name is mandatory")
     @Size(max = 50, message = "Team: name must be at most 50 characters")
     private String name;
 
-    
+    // standard constructors / setters / getters / toString
+    public Team() {}
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamId=" + teamId +
+                ", name='" + name + '\'' +
+                '}';
+    }
     
 }
