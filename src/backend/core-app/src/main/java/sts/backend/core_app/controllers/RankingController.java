5ed3ebf4833f8edbd5cdedc1a31773081a70bbcd
Pedro Pinto <pmap@ua.pt>
2024-11-03 23:11:16 +0000

    ranking done

--- src/backend/core-app/src/main/java/sts/backend/core_app/controllers/RankingController.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/controllers/RankingController.java
@@ -1,12 +1,31 @@
 package sts.backend.core_app.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sts.backend.core_app.dto.player.PlayerIdWithTimeOption;
+import sts.backend.core_app.services.business.RankingService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RankingController {
     
     private final RankingService rankingService;
+
+    public RankingController(RankingService rankingService) {
+        this.rankingService = rankingService;
+    }
+
+    @GetMapping("/ranking/sleep")
+    public Long api_get_sleep_ranking(@RequestBody PlayerIdWithTimeOption playerIdWithTimeOption) {
+        return rankingService.getSleepRanking(playerIdWithTimeOption); // TODO: implement
+    }
+
+    @GetMapping("/ranking/recovery-strain")
+    public Long api_get_recovery_strain_ranking(@RequestBody PlayerIdWithTimeOption playerIdWithTimeOption) {
+        return rankingService.getRecoveryStrainRanking(playerIdWithTimeOption); // TODO: implement
+    }
 
 }
--- src/backend/core-app/src/main/java/sts/backend/core_app/services/business/RankingService.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/services/business/RankingService.java
@@ -1,5 +1,17 @@
 package sts.backend.core_app.services.business;
 
+import sts.backend.core_app.dto.player.PlayerIdWithTimeOption;
+
 public class RankingService {
+
+    public Long getSleepRanking(PlayerIdWithTimeOption playerIdWithTimeOption) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSleepRanking'");
+    }
+
+    public Long getRecoveryStrainRanking(PlayerIdWithTimeOption playerIdWithTimeOption) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRecoveryStrainRanking'");
+    }
 
 }
