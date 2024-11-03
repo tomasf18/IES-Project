e95183fe75f2bad579f6a164e9034d4517303952
Joao <jpapinto@ua.pt>
2024-11-03 23:35:45 +0000

    delete user

--- src/backend/core-app/src/main/java/sts/backend/core_app/persistence/RelationalQueriesImpl.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/persistence/RelationalQueriesImpl.java
@@ -168,14 +168,14 @@ public class RelationalQueriesImpl implements RelationalQueries {
             throw new ResourceNotFoundException("User not found");
         }
     
-        // delete related entities
-        playerRepository.findById(userId).ifPresent(player -> {
-            playerSessionRepository.deleteByPlayerId(player.getUserId());
-            // playerSensorRepository.deleteByPlayerId(player.getId());
-            playerRepository.delete(player);
-        });
+        // TODO: verify if is necessary to delete related entities
+        // playerRepository.findById(userId).ifPresent(player -> {
+        //     playerSessionRepository.deleteByPlayerId(player.getUserId());
+        //     // playerSensorRepository.deleteByPlayerId(player.getId());
+        //     playerRepository.delete(player);
+        // });
     
-        userRepository.deleteById(userId);
+        userRepository.deleteById(userId); 
     }
 
     public void deleteTeam(Long teamId) {
