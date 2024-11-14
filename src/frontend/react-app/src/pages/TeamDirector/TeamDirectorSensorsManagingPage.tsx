f0a1f0e56ef2d502bf6a0577cf4c8d0c5d40c8e8
DaniloMicael <danilomc2004@gmail.com>
2024-11-14 09:53:09 +0000

    frontend: Team Director Sensors Managing Page

--- src/frontend/react-app/src/pages/TeamDirector/TeamDirectorSensorsManagingPage.tsx
+++ src/frontend/react-app/src/pages/TeamDirector/TeamDirectorSensorsManagingPage.tsx
@@ -1,4 +1,9 @@
-import { SideBar, Header } from "../../components";
+import {
+  SideBar,
+  Header,
+  ConfigurationCard,
+  StripedTable,
+} from "../../components";
 import { FaUsers, FaHeartPulse } from "react-icons/fa6";
 import { useLocation } from "react-router-dom";
 
@@ -24,6 +29,36 @@ export default function TeamDirectorSensorsManagingPage() {
     color: "primary" | "secondary";
   }[] = [{ to: "/", label: "Sign Out", color: "primary" }];
 
+  let StripedTableWidthClass = "w-full";
+  let StripedTableHeightClass = "h-full";
+  let StripedTableColumnsName = [
+    "SensorId",
+    "State",
+    "Player",
+    "Option",
+  ];
+  let StripedTableRows = [
+    ['9F2X4WQ8JH', "On", "Danilo Silva", "Remove"],
+    ["9F2X4WQ8JH", "Off", "Pedro Pinto", "Remove"],
+    ["9F2X4WQ8JH", "On", "João Pinto", "Remove"],
+    ["9F2X4WQ8JH", "On", "Tomás Fernandes", "Remove"],
+    ["9F2X4WQ8JH", "Off", "Guilherme Ferreira", "Remove"],
+    ["9F2X4WQ8JH", "Off", "Jorge", "Remove"],
+    ["9F2X4WQ8JH", "Off", "Daniel Silva", "Remove"],
+  ];
+
+  let configurationCardWidthClass = "w-[80rem]";
+  let configurationCardHeightClass = "h-[50rem]";
+  let configurationCardName = "António Mendes";
+  let configurationCardRightContent = (
+    <StripedTable
+      widthClass={StripedTableWidthClass}
+      heightClass={StripedTableHeightClass}
+      columnsName={StripedTableColumnsName}
+      rows={StripedTableRows}
+    />
+  );
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header buttons={headerButtons} />
@@ -35,7 +70,17 @@ export default function TeamDirectorSensorsManagingPage() {
           activePath={location.pathname}
         />
         {/* Main Content */}
-        <div className="flex-grow p-8">{/* Content */}</div>
+        <div className="flex-grow flex flex-col p-8 jsutify-center items-center">
+          <div className="w-full max-w-[80rem]">
+            <h2 className="text-2xl mb-6 text-left">Sensors Managing</h2>
+          </div>
+          <ConfigurationCard
+            widthClass={configurationCardWidthClass}
+            heightClass={configurationCardHeightClass}
+            name={configurationCardName}
+            rightContent={configurationCardRightContent}
+          />
+        </div>
       </div>
     </div>
   );
