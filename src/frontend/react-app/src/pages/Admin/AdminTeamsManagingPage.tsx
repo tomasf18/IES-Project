c549d93fd9a44636d2a8d054c933361f49a7c3ae
Joao <jpapinto@ua.pt>
2024-11-16 14:54:28 +0000

    feature: admin teams managing done

--- src/frontend/react-app/src/pages/Admin/AdminTeamsManagingPage.tsx
+++ src/frontend/react-app/src/pages/Admin/AdminTeamsManagingPage.tsx
@@ -1,6 +1,7 @@
-import { SideBar, Header } from "../../components";
+import { SideBar, Header, TeamsCard } from "../../components";
 import { FaUsers, FaCode, FaHeartPulse } from "react-icons/fa6";
 import { useLocation } from "react-router-dom";
+import { Google } from "../../assets";
 
 export default function AdminTeamsManagingPage() {
   const navLinks = [
@@ -29,6 +30,56 @@ export default function AdminTeamsManagingPage() {
     color: "primary" | "secondary";
   }[] = [{ to: "/", label: "Sign Out", color: "primary" }];
 
+  // Add teams array
+  const teams = [
+    {
+      teamName: "Real Madrid",
+      numberTeamMembers: 10,
+      teamPhotoURL: Google,
+      teamId: "team-1231",
+    },
+    {
+      teamName: "Barcelona",
+      numberTeamMembers: 12,
+      teamPhotoURL: Google,
+      teamId: "team-1232",
+    },
+    {
+      teamName: "Atletico Madrid",
+      numberTeamMembers: 8,
+      teamPhotoURL: Google,
+      teamId: "team-1233",
+    },
+    {
+      teamName: "Atletico Madrid",
+      numberTeamMembers: 8,
+      teamPhotoURL: Google,
+      teamId: "team-1233",
+    },
+    {
+      teamName: "Atletico Madrid",
+      numberTeamMembers: 8,
+      teamPhotoURL: Google,
+      teamId: "team-1233",
+    },
+    {
+      teamName: "Atletico Madrid",
+      numberTeamMembers: 8,
+      teamPhotoURL: Google,
+      teamId: "team-1233",
+    },
+    {
+      teamName: "Atletico Madrid",
+      numberTeamMembers: 8,
+      teamPhotoURL: Google,
+      teamId: "team-1233",
+    },
+  ];
+
+  const handleTeamManagement = (teamId: string) => {
+    console.log(`Managing team with id: ${teamId}`);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header buttons={headerButtons} />
@@ -40,7 +91,9 @@ export default function AdminTeamsManagingPage() {
           activePath={location.pathname}
         />
         {/* Main Content */}
-        <div className="flex-grow p-8">{/* Content */}</div>
+        <div className="flex-grow p-8">
+            <TeamsCard teams={teams} handleTeamManagement={handleTeamManagement} />
+        </div>
       </div>
     </div>
   );
--- src/frontend/react-app/src/pages/TestPage.tsx
+++ src/frontend/react-app/src/pages/TestPage.tsx
@@ -1,75 +1,15 @@
-import { useEffect, useState } from 'react';
-import PlayersCard from "../components/PlayersCard/PlayersCard";
-import { Google } from "../assets";
+import { Header } from "../components";
 
 export default function TestPage() {
-  const [players, setPlayers] = useState([
-    {
-      playerPhotoURL: Google,
-      playerName: "Heart Rate",
-      playerId: "123",
-      singleValue: "150",
-      actualState: "Normal",
-      color: "red",
-      values: ["150", "170", "130", "123", "177", "190", "145", "210"],
-      metric: "bpm"
-    }, 
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "1123",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "green",
-      values: ["123", "124", "67", "123", "145", "167", "145", "210"],
-      metric: "bpm"
-    },
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "112223",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "blue",
-      values: ["123", "124", "67", "123", "145", "167", "145", "210"],
-      metric: "bpm"
-    },
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "11323",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "red",
-      values: [],
-      metric: "bpm"
-    },
-  ]);
+    const headerButtons: {
+        to: string;
+        label: string;
+        color: "primary" | "secondary";
+      }[] = [{ to: "/", label: "Sign Out", color: "primary" }];
 
-  // Add random value every 3 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPlayers((prevPlayers) => {
-        const newPlayers = [...prevPlayers]; 
-        // newPlayers.forEach(player => player.values.push((Math.floor(Math.random() * 100) + 100).toString())); // Add new random value as a string
-        // add new random value as a string for the first player
-        if (newPlayers[0].values) {
-          newPlayers[0].values.push((Math.floor(Math.random() * 100) + 100).toString());
-        }
-        return newPlayers;
-      });
-    }, 300);
-
-    return () => clearInterval(interval); // Clear the interval on component unmount
-  }, []);
-
-const handlePlayerManagement = (playerId: string) => {
-  console.log(`Player with id: ${playerId}`);
-};
-
-return (
-    <div className="flex flex-wrap justify-center p-4">
-      <PlayersCard players={players} handlePlayerManagement={handlePlayerManagement} />
-    </div>
-  );
+    return (
+        <div>
+            <Header buttons={headerButtons} />
+        </div>
+    );
 }
