f4938ff3123c6502a66ee6eb7fbd2c74c2aa83b3
Joao <jpapinto@ua.pt>
2024-11-16 11:42:58 +0000

    navbar done

--- src/frontend/react-app/src/pages/TestPage.tsx
+++ src/frontend/react-app/src/pages/TestPage.tsx
@@ -3,73 +3,32 @@ import PlayersCard from "../components/PlayersCard/PlayersCard";
 import { Google } from "../assets";
 
 export default function TestPage() {
-  const [players, setPlayers] = useState([
-    {
-      playerPhotoURL: Google,
-      playerName: "Heart Rate",
-      playerId: "123",
-      singleValue: "150",
-      actualState: "Normal",
-      color: "red",
-      values: ["150", "170", "130", "123", "177", "190", "145", "210"],
-      metric: "bpm"
-    }, 
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "1123",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "green",
-      values: ["123", "124", "67", "123", "145", "167", "145", "210"],
-      metric: "bpm"
-    },
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "112223",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "blue",
-      values: ["123", "124", "67", "123", "145", "167", "145", "210"],
-      metric: "bpm"
-    },
-    {
-      playerPhotoURL: Google,
-      playerName: "João Pinto",
-      playerId: "11323",
-      singleValue: "150",
-      actualState: "Critical",
-      color: "red",
-      values: [],
-      metric: "bpm"
-    },
-  ]);
+  const [activeOption, setActiveOption] = useState('GET');
 
-  // Add random value every 3 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPlayers((prevPlayers) => {
-        const newPlayers = [...prevPlayers]; 
-        // newPlayers.forEach(player => player.values.push((Math.floor(Math.random() * 100) + 100).toString())); // Add new random value as a string
-        // add new random value as a string for the first player
-        if (newPlayers[0].values) {
-          newPlayers[0].values.push((Math.floor(Math.random() * 100) + 100).toString());
-        }
-        return newPlayers;
-      });
-    }, 300);
+    const handleOptionClick = (option: string) => {
+        setActiveOption(option);
+    };
 
-    return () => clearInterval(interval); // Clear the interval on component unmount
-  }, []);
-
-const handlePlayerManagement = (playerId: string) => {
-  console.log(`Player with id: ${playerId}`);
-};
-
-return (
-    <div className="flex flex-wrap justify-center p-4">
-      <PlayersCard players={players} handlePlayerManagement={handlePlayerManagement} />
-    </div>
-  );
+    return (
+        <div className="flex justify-around bg-purple-50 py-2 border-b-2 border-gray-200 w-1/4 mx-auto mt-4 rounded-lg shadow-md">
+            {['GET', 'POST', 'PUT', 'DELETE'].map((option) => (
+                <div
+                    key={option}
+                    onClick={() => handleOptionClick(option)}
+                    className={`text-lg font-semibold cursor-pointer ${
+                        activeOption === option ? 'text-purple-700' : 'text-gray-600'
+                    }`}
+                    style={{ position: 'relative' }}
+                >
+                    {option}
+                    {activeOption === option && (
+                        <div
+                            className="absolute bottom-0 left-0 right-0 h-1 bg-purple-700"
+                            style={{ width: '100%' }}
+                        />
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 }
