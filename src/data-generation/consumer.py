96820a3034dcd1bba6e4b2821c74a8c1dc520dbc
Joao <jpapinto@ua.pt>
2024-11-26 18:19:06 +0000

    python generator working; consumer and producer and plotGraphs example

--- src/data-generation/consumer.py
+++ src/data-generation/consumer.py
@@ -12,16 +12,12 @@ from kafka import TopicPartition
 
 # Kafka Configuration
 bootstrap_servers = 'localhost:29092'  # Adjust if needed
-nMec = 104384  # Replace with your actual nMec number
-topic_name = f'lab05_{nMec}'
-group_id = f'lab05_consumer_{nMec}'  # Unique group ID
-# group_id = 'new_unique_group_id'
+topic_name = f'players_data_real_time'
 
 # Create a Kafka consumer instance
 consumer = KafkaConsumer(
     topic_name,
     bootstrap_servers=bootstrap_servers,
-    group_id=group_id,
     enable_auto_commit=True,
     auto_offset_reset='earliest',  # Start consuming from the beginning of the topic if no offset is stored
 )
@@ -38,4 +34,9 @@ print(f"Consuming messages from topic '{topic_name}':")
 # Consume messages
 for message in consumer:
     msg = message.value
-    print(f"Received message: {msg}")
+    print(f"Message: {msg}")
+
+    # Store the message in a file
+    with open('consumed_messages.txt', 'a') as file:
+        file.write(f"{msg}\n")
+
--- src/data-generation/producer.py
+++ src/data-generation/producer.py
@@ -11,9 +11,17 @@ import json
 import time
 import random
 
+# Check if sensors IDs are provided
+if len(sys.argv) < 2:
+    print("Usage: producer.py sensor_id1 [sensor_id2 ...]")
+    sys.exit(1)
+
+# Get sensor IDs from command line arguments
+sensor_ids = [int(id_str) for id_str in sys.argv[1:]]
+
 # Kafka Configuration
 bootstrap_servers = 'localhost:29092'  # Adjust if your Kafka broker is at a different address
-topic_name = f'lab05_quotes_real_time'
+topic_name = f'players_data_real_time'
 
 # Initialize Kafka Producer with JSON serialization
 producer = KafkaProducer(
@@ -21,33 +29,38 @@ producer = KafkaProducer(
     value_serializer=lambda m: json.dumps(m).encode('utf-8')
 )
 
-# 
-averageHeartRate = 160.0 # bmp
-averageRespiratoryRate = 16.0 # rpm
-averageBodyTemperature = 36.5 # ºC
+# Average Heart Rate, Respiratory Rate, and Body Temperature
+average_heart_rate = 160.0 # bmp
+average_respiratory_rate = 16.0 # rpm
+average_body_temperature = 36.5 # ºC
 
 # Standard deviation
-standardDeviationHeartRate = 20.0
-standardDeviationRespiratoryRate = 2.0
-standardDeviationBodyTemperature = 0.5
+standard_deviation_heart_rate = 10.0
+standard_deviation_respiratory_rate = 2.0
+standard_deviation_body_temperature = 0.2
 
 # Generate new quotes for movies every 5 to 10 seconds and introduce them in a Kafka topic. 
 try:
     while True:
-        # Generate random values following a normal distribution
-        heartRate = random.gauss(averageHeartRate, standardDeviationHeartRate)
-        respiratoryRate = random.gauss(averageRespiratoryRate, standardDeviationRespiratoryRate)
-        bodyTemperature = random.gauss(averageBodyTemperature, standardDeviationBodyTemperature)
+        message_list = []
+        for sensor_id in sensor_ids:
+            # Generate random values following a normal distribution
+            heartRate = random.gauss(average_heart_rate, standard_deviation_heart_rate)
+            respiratoryRate = random.gauss(average_respiratory_rate, standard_deviation_respiratory_rate)
+            bodyTemperature = random.gauss(average_body_temperature, standard_deviation_body_temperature)
 
-        message = {
-            'heartRate': heartRate,
-            'respiratoryRate': respiratoryRate,
-            'bodyTemperature': bodyTemperature
-        }
-        producer.send(topic_name, value=message)
-        print(f"Produced: {message}")
-
-        time.sleep(random.uniform(5, 10))
+            sensor_data = {
+                'sensor_id': sensor_id,
+                'heart_rate': heartRate,
+                'respiratory_rate': respiratoryRate,
+                'body_temperature': bodyTemperature
+            }
+            message_list.append(sensor_data)
+        
+        # Send the list of sensor data as a single message
+        producer.send(topic_name, value=message_list)
+        print(f"Produced: {message_list}")
+        time.sleep(random.uniform(0.5, 1.0))
 
 except KeyboardInterrupt:
     print("Stopping producer...")
