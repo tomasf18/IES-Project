b1eaa6039e562759eb03b2db21f4020a4c8f9300
DaniloMicael <danilomc2004@gmail.com>
2024-11-26 17:51:39 +0000

    frontend: SignUp Validation

--- src/frontend/react-app/src/pages/LandingPage.tsx
+++ src/frontend/react-app/src/pages/LandingPage.tsx
@@ -17,34 +17,40 @@ export default function LandingPage() {
     { href: "#security", label: "Security" },
   ];
 
-  const buttons: { to: string; label: string; color: "primary" | "secondary" }[] = [
+  const buttons: {
+    to: string;
+    label: string;
+    color: "primary" | "secondary";
+  }[] = [
     { to: "/login", label: "Login", color: "secondary" },
     { to: "/signup", label: "Sign Up", color: "primary" },
   ];
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header links={links} buttons={buttons}/>
+      <Header links={links} buttons={buttons} />
 
-      <main className="flex-grow">
-        <section id="home">
-          <HomeSection />
-        </section>
-        <section id="service">
-          <ServiceSection />
-        </section>
-        <div className="pr-16 pl-16">
-          <hr className="border-2 border-gray-200" />
+      <main className="flex-grow pb-80">
+        <div className="transform scale-110 origin-top space-y-8">
+          <section id="home">
+            <HomeSection />
+          </section>
+          <section id="service">
+            <ServiceSection />
+          </section>
+          <div className="pr-16 pl-16">
+            <hr className="border-2 border-gray-200" />
+          </div>
+          <section id="feature">
+            <FeatureSection />
+          </section>
+          <section id="product">
+            <ProductSection />
+          </section>
+          <section id="security">
+            <SecuritySection />
+          </section>
         </div>
-        <section id="feature">
-          <FeatureSection />
-        </section>
-        <section id="product">
-          <ProductSection />
-        </section>
-        <section id="security">
-          <SecuritySection />
-        </section>
       </main>
 
       <Footer />
