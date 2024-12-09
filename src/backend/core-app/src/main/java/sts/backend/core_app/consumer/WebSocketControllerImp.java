1c5b0bee4f0a20f421d8885555e00015f69559ab
Tomás Santos <t.santos.f@ua.pt>
2024-12-09 23:46:43 +0000

    first 2 websockets

--- src/backend/core-app/src/main/java/sts/backend/core_app/consumer/WebSocketControllerImp.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/consumer/WebSocketControllerImp.java
@@ -7,6 +7,8 @@ import sts.backend.core_app.dto.session.RealTimeExtraDetailsResponse;
 import sts.backend.core_app.dto.session.RealTimeInfoResponse;
 import sts.backend.core_app.dto.team.PlayersAvailableRealTimeInfo;
 import sts.backend.core_app.consumer.interfaces.WebSocketController;
+import sts.backend.core_app.dto.admin.SensorsTeamWeek;
+import sts.backend.core_app.dto.admin.SensorsLast5Days;
 
 @Controller
 public class WebSocketControllerImp implements WebSocketController {
@@ -26,5 +28,13 @@ public class WebSocketControllerImp implements WebSocketController {
 
     public void sendPlayersRealTimeExtraDetails(Long playerId, RealTimeExtraDetailsResponse realTimeExtraDetailsResponse) {
         messagingTemplate.convertAndSend("/topic/realTimeExtraDetails/" + playerId, realTimeExtraDetailsResponse);
+    }
+
+    public void sendSensorsTeamWeek(List<SensorsTeamWeek> sensorsTeamWeek) {
+        messagingTemplate.convertAndSend("/topic/sensorsTeamWeek", sensorsTeamWeek);
+    }
+
+    public void sendSensorsLast5Days(List<SensorsLast5Days> sensorsLast5Days) {
+        messagingTemplate.convertAndSend("/topic/sensorsLast5Days", sensorsLast5Days);
     }
 }
--- src/backend/core-app/src/main/java/sts/backend/core_app/consumer/interfaces/WebSocketController.java
+++ src/backend/core-app/src/main/java/sts/backend/core_app/consumer/interfaces/WebSocketController.java
@@ -2,6 +2,8 @@ package sts.backend.core_app.consumer.interfaces;
 
 import java.util.List;
 
+import sts.backend.core_app.dto.admin.SensorsLast5Days;
+import sts.backend.core_app.dto.admin.SensorsTeamWeek;
 import sts.backend.core_app.dto.session.RealTimeExtraDetailsResponse;
 import sts.backend.core_app.dto.session.RealTimeInfoResponse;
 import sts.backend.core_app.dto.team.PlayersAvailableRealTimeInfo;
@@ -10,4 +12,6 @@ public interface WebSocketController {
     public void sendRealTimeInfo(Long sessionId, RealTimeInfoResponse realTimeInfo);
     public void sendPlayersAvailableRealTimeInfo(Long teamId, List<PlayersAvailableRealTimeInfo> playersAvailableRealTimeInfo);
     public void sendPlayersRealTimeExtraDetails(Long playerId, RealTimeExtraDetailsResponse realTimeExtraDetailsResponse);
+    public void sendSensorsTeamWeek(List<SensorsTeamWeek> sensorsTeamWeek);
+    public void sendSensorsLast5Days(List<SensorsLast5Days> sensorsLast5Days);
 }
